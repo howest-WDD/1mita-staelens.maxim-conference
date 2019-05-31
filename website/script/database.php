d63962c85a0b2a6e0f2cf8c07e2d4219f8761426
maximstaelens <32571591+maximstaelens@users.noreply.github.com>
2019-05-31 13:21:10 +0200

    Aanmaken database.php

---
+++ website/script/database.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// Hostname: localhost, username: your_user, password: your_pass, db: MyDatabase
+$mysqli= new mysqli('localhost', 'conference', 'conference', 'conference');
+
+// Oh no! Wanneer er een connect_errno is, hebben we geen verbinding!
+if ($mysqli->connect_errno) {
+    // In dit geval laat je de gebruiker best iets weten
+    echo "Sorry, this website is experiencing connection problems.";
+    // Dit doe je best niet op een publieke website
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_errorh . "\n";
+
+    // Je zou de gebruiker naar een mooie foutpagina kunnen brengen, of gewoon
+    // stoppen
+    exit();
+}
+
+?>
